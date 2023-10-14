8a8972c0eacb02ca6b601b0ead727e7c58cf58a6
PedroR_S <pedroxbrs@gmail.com>
2023-10-13 22:11:38 -0300

    fix bugs and interferances for the deploy

---
+++ to-do-list/src/main/java/br/com/pedrorodrigues/todolist/errors/RespondeEntity.java
@@ -0,0 +1,11 @@
+package br.com.pedrorodrigues.todolist.errors;
+
+import org.springframework.http.HttpStatus;
+
+public class RespondeEntity<T> {
+
+    public static Object status(HttpStatus badRequest) {
+        return null;
+    }
+
+}
---
+++ to-do-list/src/main/java/br/com/pedrorodrigues/todolist/tasks/RespondeEntity.java
@@ -0,0 +1,5 @@
+package br.com.pedrorodrigues.todolist.tasks;
+
+public class RespondeEntity {
+
+}
